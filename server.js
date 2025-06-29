1653ff82896f9ee821dc79f05fd4a780b410d303
cccp200 <63626995+cccp200@users.noreply.github.com>
2025-06-29 21:13:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure Node.js Web App!');
+  res.send('Hello from my Azure Node.js Web App!');
 });
 
 app.listen(port, () => {
